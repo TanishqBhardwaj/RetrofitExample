491cb5bfd0e7040785e939106afd92226817d19d
TanishqBhardwaj <47564288+TanishqBhardwaj@users.noreply.github.com>
2021-06-04 21:03:46 +0530

    Add dynamic country and state cards

--- app/src/main/java/com/example/retrofitexample/activity/DetailActivity.java
+++ app/src/main/java/com/example/retrofitexample/activity/DetailActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 import com.example.retrofitexample.local.StateDAO;
 import com.example.retrofitexample.local.StateDatabase;
 import com.example.retrofitexample.local.StateEntity;
+import com.example.retrofitexample.model.CountriesModel;
+import com.example.retrofitexample.model.GlobalCasesModel;
 import com.example.retrofitexample.network.JsonApiHolder;
 import com.example.retrofitexample.model.LatestCasesModel;
 import com.example.retrofitexample.R;
@@ -31,6 +33,7 @@ public class DetailActivity extends AppCompatActivity{
     private RecyclerView recyclerView;
     private JsonApiHolder jsonApiHolder;
     private List<RegionalData> stateWiseCaseList = new ArrayList<>();
+    private List<CountriesModel> countriesWiseCaseList = new ArrayList<>();
 
     private StateDatabase stateDatabase;
     private StateDAO stateDAO;
@@ -42,22 +45,23 @@ public class DetailActivity extends AppCompatActivity{
 
         setView();
 
-        jsonApiHolder = RetrofitClass.getRetrofitInstance(Constants.BASE_LOCAL_URL).create(JsonApiHolder.class);
         stateDatabase = StateDatabase.getInstance(this);
         stateDAO = stateDatabase.stateDAO();
-        getLatestCases();
+        getLatestStateCases();
+//        getLatestCountryCases();
     }
 
     private void setView() {
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL, false));
-        DetailAdapter detailAdapter = new DetailAdapter(stateWiseCaseList);
-        recyclerView.setAdapter(detailAdapter);
+//        DetailAdapter detailAdapter = new DetailAdapter(stateWiseCaseList);
+//        recyclerView.setAdapter(detailAdapter);
 
     }
 
-    private void getLatestCases() {
+    private void getLatestStateCases() {
+        jsonApiHolder = RetrofitClass.getRetrofitInstance(Constants.BASE_LOCAL_URL).create(JsonApiHolder.class);
         Call<LatestCasesModel> latestCasesModel = jsonApiHolder.getLatestCases();
 
         latestCasesModel.enqueue(new Callback<LatestCasesModel>() {
@@ -86,6 +90,28 @@ public class DetailActivity extends AppCompatActivity{
                         t.printStackTrace();
                     }
                 });
+    }
+
+    private void getLatestCountryCases() {
+        jsonApiHolder = RetrofitClass.getRetrofitInstance(Constants.BASE_GLOBAL_URL).create(JsonApiHolder.class);
+        Call<GlobalCasesModel> globalCasesModelCall = jsonApiHolder.getGlobalCases();
+
+        globalCasesModelCall.enqueue(new Callback<GlobalCasesModel>() {
+            @Override
+            public void onResponse(Call<GlobalCasesModel> call, Response<GlobalCasesModel> response) {
+                if(response.isSuccessful()) {
+                    countriesWiseCaseList.addAll(response.body().getCountries());
+
+                    DetailAdapter detailAdapter = new DetailAdapter(countriesWiseCaseList, true);
+                    recyclerView.setAdapter(detailAdapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GlobalCasesModel> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
     private void saveInDb() {
--- app/src/main/java/com/example/retrofitexample/adapter/DetailAdapter.java
+++ app/src/main/java/com/example/retrofitexample/adapter/DetailAdapter.java
@@ -6,9 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.retrofitexample.R;
+import com.example.retrofitexample.model.CountriesModel;
 import com.example.retrofitexample.model.RegionalData;
 
 import java.util.ArrayList;
@@ -17,10 +19,18 @@ import java.util.List;
 public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.StateViewHolder>{
 
     private List<RegionalData> stateList;
+    private List<CountriesModel> countriesList;
     private OnItemClickListener mListener;
+    private boolean isGlobal;
 
     public DetailAdapter(List<RegionalData> tempList) {
         stateList = new ArrayList<>(tempList);
+        isGlobal = false;
+    }
+
+    public DetailAdapter(List<CountriesModel> countriesModelList, boolean isGlobal) {
+        countriesList = new ArrayList<>(countriesModelList);
+        this.isGlobal = isGlobal;
     }
 
     public interface OnItemClickListener {
@@ -42,17 +52,41 @@ public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.StateViewH
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull StateViewHolder holder, int position) {
-        RegionalData regionalData = stateList.get(position);
-        holder.textViewState.setText(regionalData.getStateName());
-        holder.textViewTotal.setText("Total Cases = " + regionalData.getTotalConfirmed());
-        holder.textViewDeath.setText("Number of Deaths = " + regionalData.getDeaths());
-        holder.textViewRecovered.setText("Recovered Cases = " + regionalData.getDischarged());
-        holder.textViewActive.setText("Active Cases = " +
-                (regionalData.getTotalConfirmed() - regionalData.getDischarged() - regionalData.getDeaths()));
+        if(isGlobal) {
+            CountriesModel countriesModel = countriesList.get(position);
+            holder.textViewName.setText(countriesModel.getCountryName());
+            holder.textViewTotal.setText(String.valueOf(countriesModel.getTotalConfirmedCases()));
+            holder.textViewDeath.setText(String.valueOf(countriesModel.getTotalDeathsCases()));
+            holder.textViewRecovered.setText(String.valueOf(countriesModel.getTotalRecoveredCases()));
+            holder.textViewActive.setText(
+                    String.valueOf(countriesModel.getTotalConfirmedCases()
+                            - countriesModel.getTotalRecoveredCases()
+                            - countriesModel.getTotalDeathsCases()));
+
+            holder.textViewTotalNew.setText(String.valueOf(countriesModel.getTotalConfirmedCases()));
+            holder.textViewRecoveredNew.setText(String.valueOf(countriesModel.getNewRecoveredCases()));
+            holder.textViewDeathNew.setText(String.valueOf(countriesModel.getNewDeathsCases()));
+        }
+        else {
+            RegionalData regionalData = stateList.get(position);
+            holder.textViewName.setText(regionalData.getStateName());
+            holder.textViewTotal.setText(String.valueOf(regionalData.getTotalConfirmed()));
+            holder.textViewDeath.setText(String.valueOf(regionalData.getDeaths()));
+            holder.textViewRecovered.setText(String.valueOf(regionalData.getDischarged()));
+            holder.textViewActive.setText(
+                    String.valueOf(regionalData.getTotalConfirmed() - regionalData.getDischarged() - regionalData.getDeaths()));
+
+            holder.constraintLayoutTotalNew.setVisibility(View.GONE);
+            holder.constraintLayoutRecoveredNew.setVisibility(View.GONE);
+            holder.constraintLayoutDeathNew.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public int getItemCount() {
+        if(isGlobal) {
+            return countriesList.size();
+        }
         return stateList.size();
     }
 
@@ -62,7 +96,14 @@ public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.StateViewH
         private TextView textViewRecovered;
         private TextView textViewDeath;
         private TextView textViewActive;
-        private TextView textViewState;
+        private TextView textViewName;
+
+        private TextView textViewTotalNew;
+        private TextView textViewRecoveredNew;
+        private TextView textViewDeathNew;
+        private ConstraintLayout constraintLayoutTotalNew;
+        private ConstraintLayout constraintLayoutRecoveredNew;
+        private ConstraintLayout constraintLayoutDeathNew;
 
         public StateViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -71,7 +112,14 @@ public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.StateViewH
             textViewRecovered = itemView.findViewById(R.id.textViewRecoveredCases);
             textViewDeath = itemView.findViewById(R.id.textViewDeath);
             textViewActive = itemView.findViewById(R.id.textViewActiveCases);
-            textViewState = itemView.findViewById(R.id.textViewState);
+            textViewName = itemView.findViewById(R.id.textViewName);
+
+            textViewTotalNew = itemView.findViewById(R.id.text_view_total_new);
+            textViewRecoveredNew = itemView.findViewById(R.id.text_view_recovered_new);
+            textViewDeathNew = itemView.findViewById(R.id.text_view_death_new);
+            constraintLayoutTotalNew = itemView.findViewById(R.id.constraintLayoutTotalNew);
+            constraintLayoutRecoveredNew = itemView.findViewById(R.id.constraintLayoutRecoveredNew);
+            constraintLayoutDeathNew = itemView.findViewById(R.id.constraintLayoutDeathNew);
 
             itemView.setOnClickListener(view -> {
                 if(mListener!=null) {
